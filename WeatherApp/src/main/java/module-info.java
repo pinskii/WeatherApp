ceabcf84125c82665e225eb605d6617f35b47fb6
melina <meruusunen@MacBook-Air-3.wlan>
2022-10-26 17:32:27 +0300

    add required modules

--- WeatherApp/src/main/java/module-info.java
+++ WeatherApp/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module fi.tuni.comp_se_110.weatherapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.net.http;
+    requires java.xml;
 
     opens fi.tuni.comp_se_110.weatherapp to javafx.fxml;
     exports fi.tuni.comp_se_110.weatherapp;
